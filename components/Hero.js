ccec32c335e4ea22792b2deec93fdb83a358e6b2
bbussell <bbussell@atproperties.com>
2026-03-13 20:47:34 -0500

    Update image class

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
       <img
         src="/chicago-hero.jpg"
         alt="Chicago skyline"
-        className="absolute inset-0 w-full h-full object-cover"
+        className="absolute inset-0 w-full h-full object-cover scale-105 animate-slow-zoom"
       />
 
       <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/35 to-black/20"></div>
